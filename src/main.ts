50669aea29e73e14513581f3e665c2e4f12411d0
Logan Bickmore <loganbickmore91@gmail.com>
2020-11-20 15:43:17 -0800

    M  .github/workflows/test.yml M  package-lock.json M  package.json M  src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {exec} from '@actions/exec'
+import * as io from '@actions/io'
+import * as cp from 'child_process'
+import * as fs from 'fs'
+
+// working directory is /github/workspace
+// home is /github/home
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // Get Context
+    const {owner, repo} = github.context.repo
+    const ref = github.context.ref
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Get Inputs
+    const specFile = core.getInput('spec_file')
+    const topdir = core.getInput('_topdir')
 
-    core.setOutput('time', new Date().toTimeString())
+    await exec('rpmdev-setuptree')
+    await exec(
+      `rpmbuild -bb ${specFile} --define "_topdir /github/workspace/${topdir}"`
+    )
+    await exec(`ls /github/workspace/${topdir}`)
+    await exec(`ls /github/workspace/${topdir}/RPMS`)
+
+    //TODO: Set outputs. ref https://github.com/naveenrajm7/rpmbuild/blob/master/src/main.ts#L75-L113
   } catch (error) {
     core.setFailed(error.message)
   }
