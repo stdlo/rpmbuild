07abb583c90bc5af87ce188d74103395a79e4c5f
Logan Bickmore <loganbickmore91@gmail.com>
2020-11-20 14:59:11 -0800

    Created docker/nodejs based action
      - Build RPMs accepting a source tar archive and specfile path
      - Example project under ./example with full release workflow in
        ./.github/workflows

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,37 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { exec } from '@actions/exec'
+
+const WORKSPACE = '/github/workspace'
+const TOPDIR = 'rpmbuild'
+const TOPDIR_ABS = `${WORKSPACE}/${TOPDIR}`
+const SOURCES = `${TOPDIR_ABS}/SOURCES`
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    /* Get inputs */
+    const specFile = core.getInput('spec_file')
+    const srcArchive = core.getInput('src_archive')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    /* Setup rpmbuild topdir */
+    await exec(`mkdir -p ${SOURCES}`)
+    await exec('mv', [srcArchive, SOURCES])
 
-    core.setOutput('time', new Date().toTimeString())
+    /* Build rpm package */
+    await exec(`rpmbuild -bb ${specFile} --define="_topdir ${TOPDIR_ABS}"`)
+
+    /* Set outputs */
+    let rpm_path = ''
+    // use relative topdir so find returns a relative path for use in output
+    await exec('find', [`${TOPDIR}/RPMS`, '-type', 'f'], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          rpm_path = data.toString().trim()
+        }
+      }
+    })
+    core.setOutput('rpm_name', rpm_path.split('/').pop())
+    core.setOutput('rpm_path', rpm_path)
+    core.setOutput('rpm_content_type', 'application/octet-stream') // Content-type for Upload
   } catch (error) {
     core.setFailed(error.message)
   }
