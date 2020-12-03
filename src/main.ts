7634fe999d2056c60c161990fb8cfc56ebf8984f
Logan Bickmore <loganbickmore91@gmail.com>
2020-12-02 16:59:24 -0800

    M  .github/workflows/test.yml M  lib/main.js M  src/main.ts ?? example/.rpm/build/BUILD/ ?? example/.rpm/build/RPMS/

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ async function run(): Promise<void> {
   try {
     // Get inputs
     const specFile = core.getInput('spec_file')
-    const topdir = '/github/workspace/' + core.getInput('_topdir')
+    const topdir = `/github/workspace/${core.getInput('_topdir')}`
 
     // Build rpm package
     await exec(`rpmbuild -bb ${specFile} --define="_topdir ${topdir}"`)
 
     // Set outputs
     let rpm_path = ''
-    await exec('find', [topdir + '/RPMS', '-type', 'f'], {
+    await exec('find', [`${topdir}/RPMS`, '-type', 'f'], {
       listeners: {
         stdout: (data: Buffer) => {
           rpm_path = data.toString().trim()
