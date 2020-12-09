4f9365062a5401fe8a28aeff2f0df38ed18f7ac4
Logan Bickmore <loganbickmore91@gmail.com>
2020-12-08 20:32:55 -0800

    M  .github/workflows/test.yml M  action.yml M  lib/main.js M  src/main.ts ?? example/.rpm/build/BUILD/ ?? example/.rpm/build/RPMS/

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 import * as core from '@actions/core'
 import { exec } from '@actions/exec'
 
+const SOURCES = 'rpmbuild/SOURCES'
+
 async function run(): Promise<void> {
   try {
     // Get inputs
     const specFile = core.getInput('spec_file')
-    const topdir = core.getInput('_topdir')
+    const srcArchive = core.getInput('src_archive')
+    // const topdir = core.getInput('_topdir')
+
+    console.log(srcArchive)
+    await exec(`mkdir -p ${SOURCES}`)
+    await exec('mv', [srcArchive, SOURCES])
 
     // Build rpm package
-    await exec(`rpmbuild -bb ${specFile} --define="_topdir /github/workspace/${topdir}"`)
+    // await exec(`rpmbuild -bb ${specFile} --define="_topdir /github/workspace/${topdir}"`)
+    await exec(`rpmbuild -bb ${specFile} --define="_topdir /github/workspace/rpmbuild"`)
 
     // Set outputs
     let rpm_path = ''
-    await exec('find', [`${topdir}/RPMS`, '-type', 'f'], {
+    // await exec('find', [`${topdir}/RPMS`, '-type', 'f'], {
+    await exec('find', ['rpmbuild/RPMS', '-type', 'f'], {
       listeners: {
         stdout: (data: Buffer) => {
           rpm_path = data.toString().trim()
