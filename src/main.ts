c8020312707c4af822e8d053a9cbd22782e685c5
Logan Bickmore <loganbickmore91@gmail.com>
2020-12-08 20:52:47 -0800

    cleaned up example. updated release workflow

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,28 @@
 import * as core from '@actions/core'
 import { exec } from '@actions/exec'
 
-const SOURCES = 'rpmbuild/SOURCES'
+const WORKSPACE = '/github/workspace'
+const TOPDIR = 'rpmbuild'
+const TOPDIR_ABS = `${WORKSPACE}/${TOPDIR}`
+const SOURCES = `${TOPDIR_ABS}/SOURCES`
 
 async function run(): Promise<void> {
   try {
-    // Get inputs
+    /* Get inputs */
     const specFile = core.getInput('spec_file')
     const srcArchive = core.getInput('src_archive')
-    // const topdir = core.getInput('_topdir')
 
-    console.log(srcArchive)
+    /* Setup rpmbuild topdir */
     await exec(`mkdir -p ${SOURCES}`)
     await exec('mv', [srcArchive, SOURCES])
 
-    // Build rpm package
-    // await exec(`rpmbuild -bb ${specFile} --define="_topdir /github/workspace/${topdir}"`)
-    await exec(`rpmbuild -bb ${specFile} --define="_topdir /github/workspace/rpmbuild"`)
+    /* Build rpm package */
+    await exec(`rpmbuild -bb ${specFile} --define="_topdir ${TOPDIR_ABS}"`)
 
-    // Set outputs
+    /* Set outputs */
     let rpm_path = ''
-    // await exec('find', [`${topdir}/RPMS`, '-type', 'f'], {
-    await exec('find', ['rpmbuild/RPMS', '-type', 'f'], {
+    // use relative topdir so find returns a relative path for use in output
+    await exec('find', [`${TOPDIR}/RPMS`, '-type', 'f'], {
       listeners: {
         stdout: (data: Buffer) => {
           rpm_path = data.toString().trim()
